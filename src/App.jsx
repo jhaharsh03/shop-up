13fe47a7edb3931d30ab7f3fe9854679d5b3db3d
Harsh Jha <harshj92002@gmail.com>
2024-01-23 18:39:41 +0530

    completed till blog section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,34 @@ import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
 import Banner from "./components/Banner/Banner";
+import headphone from "./assets/hero/headphone.png"
+import Products from "./components/Products/Products";
+import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png"
 
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
+
+
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
 
 
 
@@ -21,7 +48,9 @@ function App() {
       <Category/>
       <Category2/>
       <Services />
-      <Banner/>
+      <Banner data = {BannerData}/>
+      <Products />
+      <Banner data = {BannerData2}/>
     </div>
   )
 }
